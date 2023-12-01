10cedd34728b4701e5e2bf3df594f4b600478246
CHA <145963611+Eumnya415@users.noreply.github.com>
2023-12-01 11:27:28 +0900

    FirstProject 애너테이션 실습
    FirstProject 애너테이션 실습

---
+++ FirstProject/src/main/java/polymorphism/AppleSpeaker.java
@@ -0,0 +1,23 @@
+package polymorphism;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("apple")
+@Qualifier
+public class AppleSpeaker implements Speaker{
+
+	public AppleSpeaker() {
+		System.out.println("===> AppleSpeaker 객체 생성");
+	}
+	
+	public void volumeUp() {
+		System.out.println("AppleSpeaker -- 소리 올린다.");
+	}
+	
+	public void volumeDown() {
+		System.out.println("AppleSpeaker -- 소리 내린다.");
+	}
+	
+
+}
